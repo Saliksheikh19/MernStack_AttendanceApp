96073bd1c2fe1838b3bab137a4ef3c04db9f3ef6
Saliksheikh19 <122231864+Saliksheikh19@users.noreply.github.com>
2023-12-27 05:09:09 -0800

    Update main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -19,6 +19,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store, persistor } from "./redux/store";
 import { PersistGate } from 'redux-persist/integration/react'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const rootElement = document.getElementById('myRoot'); // Change 'root' to 'myRoot'
 
@@ -28,7 +29,9 @@ root.render(
   <React.StrictMode>
   <Provider store={store}>
 <PersistGate loading={null} persistor={persistor}>
-      <App />
+       <Router>
+          <App />
+        </Router>
 </PersistGate>
 </Provider>
     </React.StrictMode>
